ad8108379559f4baefb57001cf8f06ace6969168
nguyennhutthanh <thanh.ledah@gmail.com>
2022-12-10 21:12:36 +0700

    update

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php 
     header("location:./admin/index.php");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        window.open("https://www.w3schools.com");
+    </script>
+</body>
+</html>
